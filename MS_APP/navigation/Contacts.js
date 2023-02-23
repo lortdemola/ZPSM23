84cff22b9c46db923a172cde8b718ed8db81b3ba
Dworak Kacper <shpwot123@gmail.com>
2023-02-23 15:27:59 +0100

    display users in contacts

--- MS_APP/navigation/Contacts.js
+++ MS_APP/navigation/Contacts.js
@@ -1,15 +1,31 @@
 import React, {Component  ,useEffect, useState  } from 'react';
 import {ActivityIndicator,StyleSheet, FlatList,SafeAreaView,ScrollView,View, Text} from 'react-native'
 import ListItemContacts from './Additional/ListItemContacts';
-import Users from '../data/Users';
+
 import FlotBTN from './Additional/FlotBTN';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listTodos } from '../src/graphql/queries';
 
 
 
 
 const Contacts = ({ navigation }) => {
+    const [Users,setusers]= useState([]);
    
-    
+    useEffect(()=>{ 
+        const fetchUsers = async()=>{
+          try {
+            const usersData = await API.graphql(graphqlOperation(
+                listTodos
+  
+            ))
+            setusers(usersData.data.listTodos.items)
+          } catch (error) {
+            console.warn(e);
+          }
+        }
+        fetchUsers();
+      },[])
     return (
         <View style={styles.container} >
             <FlatList 
